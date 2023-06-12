58d23988218de2387acd1854fe9df63a0779b142
kendra <kendratomala@gmail.com>
2023-06-12 09:17:10 -0700

    Initial Commit

---
+++ finalgithub/finalgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalgithubApp.swift
+//  finalgithub
+//
+//  Created by Kendra Tomala on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct finalgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
